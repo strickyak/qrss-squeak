2162875a1147811de65e078b335238f1d5c1306c
strick <strick@yak.net>
2020-04-28 19:09:47 -0700

    Updates to decon5 script

--- decon5.sh
+++ decon5.sh
@@ -7,7 +7,17 @@ D=' // //  /  /  / /   /     / /  '
 E=' /   /   //   /  /  ////  /  / '
 
 go run qrss.go \
-  -freq=1000 -mode=decon5 -dit=5 -bw=40 \
-  -tx_on=': rig T 1' -tx_off=': rig T 0' \
+  -freq=1010 -mode=decon5 -dit=5 -bw=20 \
+  -tx_on='ic-7100 T 1' -tx_off='ic-7100 T 0' \
   -- "$E,$D,$C,$B,$A" |
 	paplay --rate=44100 --channels=1 --format=s16le --raw  /dev/stdin
+
+
+# 40m:  7.038.750 + 1010
+# 30m: 10.139.000 + 1010
+
+#go run qrss.go \
+#  -freq=1000 -mode=decon5 -dit=5 -bw=40 \
+#  -tx_on='ic-7100 T 1' -tx_off='ic-7100 T 0' \
+#  -- "$E,$D,$C,$B,$A" |
+#	paplay --rate=44100 --channels=1 --format=s16le --raw  /dev/stdin
